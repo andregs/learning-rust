9f55fbd4de7535623671aca04f672e31e8ee5b13
André Silva <andregs@users.noreply.github.com>
2020-01-31 18:05:35 -0200

    chap19 advanced - functions

--- 19-advanced/types/src/main.rs
+++ 19-advanced/types/src/main.rs
@@ -2,6 +2,8 @@
 
 fn main() {
     type_aliases();
+    functions_as_pointers();
+    how_to_return_closures();
 }
 
 fn type_aliases() {
@@ -27,4 +29,39 @@ fn type_aliases() {
 
     // there's also the ! type that means 'never'
     // it's the value returned by things like 'continue' and 'panic!'
+}
+
+fn functions_as_pointers() {
+    fn add_one(x: i32) -> i32 {
+        x + 1
+    }
+
+    // we can pass functions as values, not only closures
+    fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+        f(arg) + f(arg)
+    }
+
+    let answer = do_twice(add_one, 3);
+
+    assert_eq!(answer, 8);
+    println!("The answer is {}", answer);
+
+    // functions are represented by the type 'fn'
+    // closures are represented by the traits Fn, FnMut, FnOnce
+}
+
+fn how_to_return_closures() {
+    // you could think it's like this:
+    // fn returns_closure() -> Fn(i32) -> i32 {
+    //     |x| x + 1
+    // }
+    // but Rust doesn't know the size of the closure at compile-time
+
+    // this will do:
+    fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+        Box::new(|x| x + 1)
+    }
+
+    let f = returns_closure();
+    println!("5 + 1 is {}", f(5));
 }
