7bb06c949e0f71f4a93173ff287132e93c24e5e8
André Silva <andregs@users.noreply.github.com>
2020-02-01 20:41:37 -0200

    chap20 web server  - handle request & response

--- 20-hello-server/src/main.rs
+++ 20-hello-server/src/main.rs
@@ -1,6 +1,7 @@
 // building a single-threaded web server
 
-use std::net::TcpListener;
+use std::io::prelude::*; // r/w streams
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // binding might fail, so it returns a Result<T, E>
@@ -9,10 +10,22 @@ fn main() {
     // a single stream represents one connection attempt between client & server
     for stream in listener.incoming() {
         // connection might fail due to OS reasons, like exceeding max connections
-        let _stream = stream.unwrap();
-        println!("Connection established!");
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
 
     // we already can "cargo run" and browse to http://127.0.0.1:7878
-    // depending on your browser you'll see one or more "Connection established!" on the console output
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    // to debug request details
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    // two blank lines for headers and body
+    let response = "HTTP/1.1 204 No Content\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
